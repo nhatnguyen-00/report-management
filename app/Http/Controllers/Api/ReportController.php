2573b0957664f9c2d637ac817f2e9a87b7dbb974
nhatnguyen-00 <nhat0x9@gmail.com>
2023-03-26 08:43:49 +0700

    feature: api report index

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Report;
 use Illuminate\Support\Carbon;
@@ -13,8 +14,9 @@ class ReportController extends Controller
         $reports = Report::whereBetween('date', [Carbon::now()->subDays(14), Carbon::now()])
             ->groupBy('date')
             ->get();
-        
-        // dd($reports);
-        return view('dashboard.report.index');
+
+        return response()->json([
+            'reports' => $reports,
+        ]);
     }
 }
