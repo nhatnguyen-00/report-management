23e207307ca96e23e2028bbe9537ce3ecb8a0d73
nhatnguyen-00 <nhat0x9@gmail.com>
2023-03-26 09:36:02 +0700

    required chart js

--- resources/views/dashboard/report/index.blade.php
+++ resources/views/dashboard/report/index.blade.php
@@ -3,7 +3,9 @@
 @section('content_header')
 <h1>Dashboard</h1>
 @stop
+@vite(['resources/js/chart/reportChart.js'])
 
 @section('content')
 <p>Welcome to this beautiful admin panel.</p>
+<canvas id="myChart"></canvas>
 @stop
